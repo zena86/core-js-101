d41f1196fdb197e015a5d4c606a490960365377e
Yauheniya Mastavaya <jk@db.by>
2023-06-02 14:06:22 +0300

    feat: solve willYouMarryMe

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,15 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise(((resolve, reject) => {
+    if (typeof isPositiveAnswer === 'boolean') {
+      resolve(isPositiveAnswer ? 'Hooray!!! She said "Yes"!' : 'Oh no, she said "No".');
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  }));
+  // throw new Error('Not implemented');
 }
 
 
