f7f1d86b1c35f34c0a54716e922113258f575033
Yauheniya Mastavaya <jk@db.by>
2023-06-02 16:44:58 +0300

    feat: solve getFastestPromise

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -79,8 +79,9 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race([...array]);
+  // throw new Error('Not implemented');
 }
 
 /**
