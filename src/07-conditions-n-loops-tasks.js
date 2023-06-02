e69ad10cc3b6ca8a63db54bf1e6c23d3803fd316
Yauheniya Mastavaya <jk@db.by>
2023-06-02 13:15:28 +0300

    feat: solve toNaryString

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -426,8 +426,9 @@ function isBracketsBalanced(str) {
  *    365, 4  => '11231'
  *    365, 10 => '365'
  */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
+function toNaryString(num, n) {
+  return num.toString(n);
+  // throw new Error('Not implemented');
 }
 
 
