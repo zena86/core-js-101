55cc74f95cb270c9a2865f6d58bbe717853c1ea9
Yauheniya Mastavaya <jk@db.by>
2023-06-02 13:32:48 +0300

    feat: solve fromJSON

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -53,8 +53,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.setPrototypeOf(JSON.parse(json), proto);
+  // throw new Error('Not implemented');
 }
 
 
