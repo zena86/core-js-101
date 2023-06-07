f6ee47cd3ba6eb07efa0a9624e9efec5ea5265fc
Yauheniya Mastavaya <jk@db.by>
2023-06-08 00:29:41 +0400

    fet: solve logger

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -154,8 +154,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = JSON.stringify(args).toString().slice(1, -1);
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return result;
+  };
+  // throw new Error('Not implemented');
 }
 
 
