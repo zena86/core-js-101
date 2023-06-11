eec909e8e81c7b7429891eead5d2d1bd3d2d2724
Yauheniya Mastavaya <jk@db.by>
2023-06-11 22:24:55 +0400

    feat: solve getIdGeneratorFunction

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -202,8 +202,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let n = startFrom - 1;
+  return () => {
+    n += 1;
+    return n;
+  };
+  // throw new Error('Not implemented');
 }
 
 
